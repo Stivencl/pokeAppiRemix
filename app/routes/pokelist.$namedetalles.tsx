adcdc2824d9d3c8a2fe56ddc3a2053acd4df50b0
Stivencl <stevfoun@gmail.com>
2024-11-01 10:45:57 -0500

    Avance1

---
+++ app/routes/pokelist.$namedetalles.tsx
@@ -0,0 +1,27 @@
+
+import { useLoaderData } from "@remix-run/react";
+import {json, LoaderFunctionArgs } from "@remix-run/node"
+import invariant from "tiny-invariant";
+
+export const loader = async (
+    {params}:LoaderFunctionArgs) => {
+      invariant(params.namedetalles, "Name is undefined")
+    const pokename = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.namedetalles}`);
+    
+    return json(await pokename.json()); 
+      
+}
+
+export default function Pokemon(){
+    const pokemon = useLoaderData<typeof loader>();
+    console.log(pokemon);
+    return(
+      <div>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <p>Altura: {pokemon.height}</p>
+            <p>Peso: {pokemon.weight}</p>
+      </div>
+             
+    );
+}
