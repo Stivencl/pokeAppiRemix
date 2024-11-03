106ecaac04db91ec74127992a52b16f5bc4ac57b
Stivencl <stevfoun@gmail.com>
2024-11-03 10:43:43 -0500

    Final

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -15,16 +15,22 @@ const list = useLoaderData<typeof loader>()
  console.log(list)
 
  return(
+    <div>
+         <div className="text-sky-400 text-9xl font-bold flex justify-center">
+               <h1 className="">Poke Api</h1>
+             </div>
+             
+             <div className="grid grid-cols-5 gap-4 bg-gradient-to-r from-gray-500 to-gray-1000 fixed top-60 left-0 right-0" >
+              {list.results.map((element: any) => (
+                 <div className="" key={element.name}>
+                    <Link className="no-underline hover:underline ... text-sky-400" to={`pokemon/${element.name}`}>{element.name}</Link>
+                 </div>
+                        
+                 ))}
+                   
+     </div>
+ 
          
-        <ul>
-            {list.results.map((element: any) => (
-              
-                <li key={element.name}>
-                    <Link to={`pokemon/${element.name}`}>{element.name}</Link>
-                    
-                </li>
-            ))}
-        </ul>
-        )
-
+    </div>
+ )
 }
