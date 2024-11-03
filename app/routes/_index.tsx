6c8c17e76dbe4efef57b53e4a7c90d61ef99b749
Stivencl <stevfoun@gmail.com>
2024-11-03 11:43:37 -0500

    Stiv: otras  mejoras

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,36 +1,63 @@
-import { json } from "@remix-run/node"
+import { ActionFunctionArgs, json } from "@remix-run/node"
 import { Link, useLoaderData } from "@remix-run/react";
 
+
+export const loader = async ({ request }: ActionFunctionArgs) => {
+    console.log(request);
+   const url = new URL(request.url);
+   const offset = Number(url.searchParams.get("offset") || 0);
+   const limit = 20; // Número de Pokémon por página
  
-export const loader = async () => {
-    const pokemonlist = await fetch("https://pokeapi.co/api/v2/pokemon/");
-    return json(await pokemonlist.json());
-
-}
-
-
-
-export default function PokeList(){
-const list = useLoaderData<typeof loader>()
- console.log(list)
-
- return(
-    <div>
-         <div className="text-sky-400 text-9xl font-bold flex justify-center">
-               <h1 className="">Poke Api</h1>
-             </div>
-             
-             <div className="grid grid-cols-5 gap-4 bg-gradient-to-r from-gray-500 to-gray-1000 fixed top-60 left-0 right-0" >
-              {list.results.map((element: any) => (
-                 <div className="" key={element.name}>
-                    <Link className="no-underline hover:underline ... text-sky-400" to={`pokemon/${element.name}`}>{element.name}</Link>
-                 </div>
-                        
-                 ))}
-                   
+   const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+   const pokemonList = await response.json();
+ 
+   return json({ pokemonList, offset });
+ };
+ 
+ export default function PokeList() {
+   const { pokemonList, offset } = useLoaderData<typeof loader>();
+ 
+   return (
+     <div>
+       <div className="text-sky-400 text-9xl font-bold flex justify-center">
+         <h1 className="">Poke Api</h1>
+       </div>
+       
+       <div className="grid grid-cols-5 gap-4 bg-gradient-to-r from-gray-500 to-gray-1000 fixed top-60 left-0 right-0">
+         {pokemonList.results.map((element: any) => (
+           <div className="" key={element.name}>
+             <Link className="no-underline hover:underline text-sky-400" to={`pokemon/${element.name}`}>
+               {element.name}
+             </Link>
+           </div>
+         ))}
+         <footer>
+         <p>Desarrollado por:<br></br> Stiven Castaño Loaiza</p>
+       </footer>
+       </div>
+ 
+       {/* Paginación */}
+       <div className="flex justify-center mt-4">
+         {offset > 0 && (
+           <Link
+             to={`?offset=${offset - 20}`}
+             className="mx-2 px-4 py-2 bg-sky-400 text-white rounded hover:bg-sky-600"
+           >
+             Anterior
+           </Link>
+         )}
+         {pokemonList.next && (
+           <Link
+             to={`?offset=${offset + 20}`}
+             className="mx-2 px-4 py-2 bg-sky-400 text-white rounded hover:bg-sky-600"
+           >
+             Siguiente
+           </Link>
+         )}
+       </div>
+       
      </div>
+   );
+ }
+
  
-         
-    </div>
- )
-}
